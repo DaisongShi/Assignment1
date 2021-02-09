ea79cd3304e179321bd7822b6add2b92849b86a2
Daisong Shi <shidaisong@gmail.com>
2021-02-08 22:40:13 -0500

    Initial Commit

---
+++ Assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by Kirito24K Shi on 2021-02-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
